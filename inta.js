6b1f7b0673cd399585708501b493ede7d4cc5de6
Jack Slide <30323358+loisoj@users.noreply.github.com>
2018-06-07 21:08:28 +0300

    Update inta.js

--- inta.js
+++ inta.js
@@ -1,4 +1,5 @@
 id = 1;
+var interval = 30;//30 sec wait
 a=document.getElementsByClassName("_5f5mN");
 function nakr() {a[0].click();id==1;}
-setInterval(nakr,30*1000);
+setInterval(nakr,interval*1000);
