5807ff995156766a07c4d956ce47131a54d82d9c
Jack Slide <30323358+loisoj@users.noreply.github.com>
2018-06-08 11:11:44 +0300

    Update inta.js

--- inta.js
+++ inta.js
@@ -1,5 +1,15 @@
 id = 1;
-var interval = 30;//30 sec wait
+function getRandomNumber(start, end, increments) {
+    var numbers = [];
+    for(var n = start; n <= end; n += increments) {
+        numbers.push(n);
+    }
+
+    var randomIndex = Math.floor(Math.random() * numbers.length);
+    return numbers[randomIndex];
+}
+
+var interval = getRandomNumber(25, 45, 1);//30 sec wait
 a=document.getElementsByClassName("_5f5mN");
 function nakr() {a[0].click();id==1;}
 setInterval(nakr,interval*1000);
